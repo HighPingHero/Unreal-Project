071a43cefe73c5492e4c29a1d5b107a61bddcfb6
HighPingHero <darrylidle@gmail.com>
2017-02-01 12:45:03 -0600

    Revert "Initialized Repo"
    This reverts commit 4d14f8983fc37e2cc2e2d9ff21ba27d693efdda5.

--- Source/Victory/Victory.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Victory.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Victory, "Victory" );
